6d3863e0706f5dd96909f2355062d71aa5573b86
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-09 19:33:22 -0700

    Update fill_model_msg.py

--- selfdrive/modeld/fill_model_msg.py
+++ selfdrive/modeld/fill_model_msg.py
@@ -110,8 +110,8 @@ def fill_model_msg(msg: capnp._DynamicStructBuilder, net_output_data: Dict[str, 
       lane_diff = net_output_data['lane_lines'][0, near_lane] - net_output_data['lane_lines'][0, far_lane]
       road_edge_diff = net_output_data['lane_lines'][0, near_lane] - net_output_data['road_edges'][0, road_edge]
 
-      y_min += np.where(lane_diff[:,0] < road_edge_diff[:,0], net_output_data['road_edges'][0, road_edge, :,0], net_output_data['lane_lines'][0, far_lane, :,0])
-      z_min += np.where(lane_diff[:,1] < road_edge_diff[:,1], net_output_data['road_edges'][0, road_edge, :,1], net_output_data['lane_lines'][0, far_lane, :,1])
+      y_min += np.where(lane_diff[:,0] < road_edge_diff[:,0], net_output_data['lane_lines'][0, far_lane, :,0], net_output_data['road_edges'][0, road_edge, :,0])
+      z_min += np.where(lane_diff[:,1] < road_edge_diff[:,1], net_output_data['lane_lines'][0, far_lane, :,1], net_output_data['road_edges'][0, road_edge, :,1])
 
       y_min /= 2
       z_min /= 2
