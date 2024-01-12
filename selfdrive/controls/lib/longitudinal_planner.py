ff42a2e067748a67244fc61a173feafc24f93f8d
cgw571004 <156157950+cgw571004@users.noreply.github.com>
2024-01-12 13:04:35 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -28,14 +28,14 @@ A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 
 # Acceleration profiles - Credit goes to the DragonPilot team!
                  # MPH = [0.,  35,   35,  40,    40,  45,    45,  67,    67,   67, 123]
-A_CRUISE_MIN_BP_CUSTOM = [0., 2.0, 2.01, 11., 11.01, 18., 18.01, 28., 28.01,  33., 55.]
+A_CRUISE_MIN_BP_CUSTOM = [0., 2.0,  11.,  18.,  28.,  33., 55.]
                  # MPH = [0., 6.71, 13.4, 17.9, 24.6, 33.6, 44.7, 55.9, 67.1, 123]
 A_CRUISE_MAX_BP_CUSTOM = [0.,    3,   6.,   8.,  11.,  15.,  20.,  25.,  30., 55.]
 
-A_CRUISE_MIN_VALS_ECO_TUNE = [-0.005, -0.010, -0.12, -0.24, -0.40, -0.36, -0.32, -0.28, -0.28, -0.25, -0.25]
+A_CRUISE_MIN_VALS_ECO_TUNE = [-0.005, -0.010, -0.12, -0.24, -0.36, -0.28, -0.25]
 A_CRUISE_MAX_VALS_ECO_TUNE = [3.5, 3.5, 1.8, 1.1, .76, .62, .47, .36, .28, .09]
 
-A_CRUISE_MIN_VALS_SPORT_TUNE = [-0.010, -0.015, -0.16, -0.28, -0.42, -0.42, -0.40, -0.35, -0.35, -0.30, -0.30]
+A_CRUISE_MIN_VALS_SPORT_TUNE = [-0.010, -0.015, -0.16, -0.28, -0.42, -0.35, -0.30]
 A_CRUISE_MAX_VALS_SPORT_TUNE = [3.5, 3.5, 3.0, 2.6, 1.4, 1.0, 0.7, 0.6, .38, .2]
 
 # Lookup table for turns
